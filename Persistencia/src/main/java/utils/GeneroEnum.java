8e180db867634f8d64fbf7f7513f244af86626b7
gaso <gustavo2840@gmail.com>
2017-02-16 19:43:59 -0500

    Implementando Entidad y pruebas de Entidad

---
+++ Persistencia/src/main/java/utils/GeneroEnum.java
@@ -0,0 +1,5 @@
+package utils;
+
+public enum GeneroEnum {
+	MASCULINO,FEMENINO
+}
---
+++ Pruebas/src/main/java/entidades/PersonaTest.java
@@ -0,0 +1,33 @@
+package entidades;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class PersonaTest {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Deployment
+
+	public static Archive<?> createTestArchive() {
+		return ShrinkWrap.create(WebArchive.class,
+				"test.war").addPackage(Persona.class.getPackage())
+				.addAsResource("persistenceForTest.xml", "META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+	
+	@Test
+	public void generacionTest() {
+	}
+
+}
