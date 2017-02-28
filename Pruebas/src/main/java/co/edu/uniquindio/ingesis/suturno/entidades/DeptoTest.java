c8c7da75dc2d90f8729b19cf3896dbc5e70b8ac3
Gustavo Salgado <gustavo2840@gmail.com>
2017-02-28 17:35:50 -0500

    Archivos json

---
+++ Pruebas/src/main/java/co/edu/uniquindio/ingesis/suturno/entidades/DeptoTest.java
@@ -0,0 +1,48 @@
+package co.edu.uniquindio.ingesis.suturno.entidades;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.persistence.UsingDataSet;
+import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class DeptoTest {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Deployment
+	public static Archive<?> createTestArchive() {
+		return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(Persona.class.getPackage())
+				.addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@Test
+	@Transactional(value = TransactionMode.ROLLBACK)
+	@UsingDataSet({ "datos/depto.json" })
+	public void findTest() {
+		Dpto dpto = entityManager.find(Dpto.class,1);
+		//Assert.assertEquals("Quindio", dpto.getNombre());
+	}
+
+	/*public void persistTest() {
+		Empleado empleado = new Empleado();
+		empleado.setCedula("423456789");
+		empleado.setNombre("Carlos Mendez");
+		empleado.setEmail("cmendez@mail.com");
+		empleado.setClave("12345");
+	}*/
+
+}
